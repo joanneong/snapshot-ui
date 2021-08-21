73117f8a9fe84f3f8cd07de9ac655ae877e728ef
joanneong <joanneongcf@u.nus.edu>
2021-08-22 00:22:36 +0800

    Create homepage

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -2,13 +2,26 @@ import Card from "../components/Card.js";
 import Header from "../components/Header.js";
 
 import "../common.css";
+import React, { useEffect, useState } from "react";
 
-function Homepage() {
+function Homepage(props) {
+  const [cardDetails, setCardDetails] = useState([]);
+
+  useEffect(() => {
+    setCardDetails([
+      {"thumbnailUrl": "https://i.ytimg.com/vi/PVjiKRfKpPI/mqdefault.jpg", "videoTitle": "Test Video 1"},
+      {"thumbnailUrl": "https://i.ytimg.com/vi/PVjiKRfKpPI/mqdefault.jpg", "videoTitle": "Test Video 2"},
+      {"thumbnailUrl": "https://i.ytimg.com/vi/PVjiKRfKpPI/mqdefault.jpg", "videoTitle": "Test Video 3"}
+    ])
+  }, []);
+
   return (
     <div className="page h-screen">
       <Header />
       <div className="grid px-7 sm:grid-cols-3 justify-center">
-        <Card thumbnailUrl="https://i.ytimg.com/vi/PVjiKRfKpPI/mqdefault.jpg" videoTitle="Video Title" />
+        {cardDetails.map(cardDetail => (
+          <Card thumbnailUrl={cardDetail.thumbnailUrl} videoTitle={cardDetail.videoTitle} />
+        ))}
       </div> 
     </div>
   );
